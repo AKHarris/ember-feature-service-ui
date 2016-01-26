fbf04beb8ed2eff42462a15ba76828db702bb5a2
Alexander Harris <harris.alexk@gmail.com>
2016-01-26 17:28:02 -0500

    adds stats, still need to backfill return*Only fields

--- addon/components/feature-service-ui.js
+++ addon/components/feature-service-ui.js
@@ -6,6 +6,10 @@ export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['feature-service-ui'],
 
+  isSpatialQuery: false,
+  displayLayerMetadata: true,
+  isStatisticsRequest: false,
+
   dataset: null,
   featureLayerUrl: '',
 
@@ -25,8 +29,28 @@ export default Ember.Component.extend({
     return this.get('dataset').attributes.url + '/query'
   }),
 
-  queryString: Ember.computed('outFields', 'f', function() {
-    return this.get('queryHost') + '?' + this.get('outFields') + '&' + this.get('f');
+  queryString: Ember.computed('where', 'objectIds', 'outFields', 'f', 'isSpatialQuery', 'geometryType', 'geometry', 'spatialRel', 'inSR', 'distance', 'units', 'returnGeometry', 'isStatisticsRequest', function() {
+    let query = this.get('queryHost') + '?' + 'where=' + this.get('where') + '&' + this.get('outFields');
+
+    if (this.get('isStatisticsRequest')) {
+      query += '&groupByFieldsForStatistics=' + this.get('groupByFieldsForStatistics');
+      query += '&outStatistics=' + this.get('outStatistics');
+    } else {
+      if (this.get('objectIds').length > 0) {
+        query += '&objectIds=' + this.get('objectIds');
+      }
+  
+      if (this.get('isSpatialQuery')) {
+        query += '&geometryType=' + this.get('geometryType');
+        query += '&geometry=' + this.get('geometry');
+        query += '&spatialRel=' + this.get('spatialRel');
+        query += '&inSR=' + this.get('inSR');
+      }
+
+    query += '&returnGeometry=' + this.get('returnGeometry');
+    }
+
+    return query + '&' + this.get('f');
   }),
 
   isAllOutFields: Ember.computed('outFields', function() {
@@ -34,10 +58,74 @@ export default Ember.Component.extend({
   }),
 
   // params
-  //
   // defaults
+  where: '1=1',
+  objectIds: '',
   outFields: 'outFields=*',
   f: 'f=json',
+
+  // spatial params
+  geometry: Ember.computed('dataset', function() {
+    let geo; 
+    geo = this.get('dataset').attributes.extent.coordinates.map(function(coord, index, array) {
+      return coord.join(',');
+    }).join(',');
+
+    return geo;
+  }),
+
+  geometryType: 'esriGeometryEnvelope', // default to 'esriGeometryEnvelope'
+  geometryTypes: [
+    'esriGeometryEnvelope',
+    'esriGeometryPolygon',
+    'esriGeometryPolyline',
+    'esriGeometryMultipoint',
+    'esriGeometryPoint'
+  ],
+ 
+  spatialRel: 'esriSpatialRelIntersects',
+  spatialRels: [
+    'esriSpatialRelIntersects',
+    'esriSpatialRelCrosses',
+    'esriSpatialRelEnvelopeIntersects',
+    'esriSpatialRelIndexIntersects',
+    'esriSpatialRelOverlaps',
+    'esriSpatialRelTouches',
+    'esriSpatialRelWithin',
+    'esriSpatialRelRelation'
+  ],
+
+  inSR: '4326', // default to 4326
+
+  distance: '',
+  units: '',
+  _units: [
+    'esriSRUnit_Meter',
+    'esriSRUnit_StatuteMile',
+    'esriSRUnit_Foot',
+    'esriSRUnit_Kilometer',
+    'esriSRUnit_NauticalMile',
+    'esriSRUnit_USNauticalMile'
+  ],
+
+  returnGeometry: true,
+
+  // TODO: params to implement
+  // 
+  // 1. maxAllowableOffset
+  // 2. geometryPrecision
+  // 3. outSR
+  // 4. gdbVersion
+  // 5. returnDistinctValues
+  // 6. returnIdsOnly
+  // 7. returnCountOnly
+  // 8. returnExtentOnly
+
+
+  orderByFields: '',
+  groupByFieldsForStatistics: '',
+  outStatistics: '',
+  // end of params
 
   actions: {
     query() {
@@ -46,15 +134,11 @@ export default Ember.Component.extend({
 
     updateOutFields(updatedField) {
       updatedField.toggleProperty('requested');
-      console.log(updatedField.get('name'));
       let outFields = '';
 
-      let requestedFields = this.get('fields').
-        filterBy('requested', true).
-        map(function(field, index, array) {
+      let requestedFields = this.get('fields').filterBy('requested', true).map(function(field, index, array) {
           return field.get('name');
-        });
-      console.log(requestedFields);
+      });
 
       if (requestedFields.length === 0) {
         outFields = 'outFields=*';
@@ -68,6 +152,36 @@ export default Ember.Component.extend({
     clearOutFields() {
       this.get('fields').setEach('requested', false);
       this.set('outFields', 'outFields=*');
+    },
+
+    toggleSpatialQuery() {
+      window.dataset = this.get('dataset');
+      this.toggleProperty('isSpatialQuery');
+    },
+
+    selectSpatialRel(rel) {
+      this.set('spatialRel', rel);
+    },
+
+    selectGeometryType(type) {
+      this.set('geometryType', type);
+    },
+
+    selectInSR(inSR) {
+      this.set('inSR', inSR);
+    },
+
+    selectUnit(unit) {
+      this.set('unit', unit);
+    },
+
+    toggleReturnGeometry() {
+      this.toggleProperty('returnGeometry');
+    },
+
+    toggleStatisticsRequest() {
+      console.log('boom');
+      this.toggleProperty('isStatisticsRequest');
     }
   },
 
